9aa8f896ee34480f4bc112130dfe0df8d8d6f677
Omar George-Elizondo <79220589+omargeorge74@users.noreply.github.com>
2021-06-25 12:40:28 -0700

    First upload

---
+++ src/Components/AddCard/index.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import { createCard } from "../../utils/api/index";
+import CardForm from "../CardForm";
+
+function AddCards() {
+      const { deckId } = useParams();
+      const [form, setForm] = useState({});
+      const history = useHistory();
+
+      const handleFormChange = (event) => {
+            setForm({
+                  ...form,
+                  deckId,
+                  [event.target.name]: event.target.value,
+            });
+      };
+
+      const handleSubmit = (event) => {
+            event.preventDefault();
+
+            async function addnewCard() {
+                  const result = await createCard(deckId, form);
+                  window.location.reload();
+                  return result;
+            }
+
+            addnewCard();
+      };
+
+      const handleDone = (event) => {
+            event.preventDefault();
+            history.push(`/decks/${deckId}`);
+      };
+
+      return (
+            <CardForm
+                  handleFormChange={handleFormChange}
+                  handleSubmit={handleSubmit}
+                  primaryBtn={"Save"}
+                  secondaryBtn={"Done"}
+                  form={form}
+                  handleDone={handleDone}
+            />
+      );
+}
+
+export default AddCards;
---
+++ src/Components/EditCard/EditCardForm.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import CardForm from "../CardForm";
+import { updateCard } from "../../utils/api/index";
+import { useHistory, useParams } from "react-router-dom";
+
+function EditCardForm({ card }) {
+      const { deckId } = useParams();
+      console.log(deckId);
+      const history = useHistory();
+      const [form, setForm] = useState({
+            front: card.front,
+            back: card.back,
+            id: card.id,
+            deckId: card.deckId,
+      });
+
+      const handleFormChange = (event) => {
+            setForm({ ...form, [event.target.name]: event.target.value });
+      };
+
+      const handleSubmit = (event) => {
+            event.preventDefault();
+
+            async function editCard() {
+                  const result = await updateCard(form);
+                  history.push(`/decks/${deckId}`);
+                  window.location.reload();
+                  return result;
+            }
+
+            editCard();
+      };
+
+      const handleDone = (event) => {
+            event.preventDefault();
+            history.push(`/decks/${deckId}`);
+      };
+
+      return (
+            <CardForm
+                  handleFormChange={handleFormChange}
+                  handleSubmit={handleSubmit}
+                  handleDone={handleDone}
+                  secondaryBtn={"Cancel"}
+                  primaryBtn={"Submit"}
+                  form={form}
+            />
+      );
+}
+
+export default EditCardForm;
